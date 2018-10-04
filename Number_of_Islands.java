59d385524876c4fb750e02d0f266aa3a6f3cde7c
Qiao He <lxheqiao@163.com>
2018-10-03 19:43:54 -0700

    Third problem

---
+++ Number_of_Islands.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    enlarge(grid, i, j);
+                }
+            }
+        }
+        return count;
+    }
+    
+    private void enlarge(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1') 
+            return;
+        
+        grid[i][j] = 0;
+        enlarge(grid, i, j + 1);
+        enlarge(grid, i, j - 1);
+        enlarge(grid, i + 1, j);
+        enlarge(grid, i - 1, j);
+               
+    }
+}
